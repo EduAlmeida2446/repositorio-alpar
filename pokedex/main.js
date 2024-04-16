111e1a8bfde046c95b1f066fa2a77b8d089762c7
Eduardo Almeida <eduardoalmeida011@hotmail.com>
2024-04-16 09:55:29 -0300

    pokedex

---
+++ pokedex/main.js
@@ -0,0 +1,22 @@
+consologe
+function showPokemon(pokemon)
+
+const text = flavorText.flavor_text
+    .replaceAll("/n",'')
+    .replaceAll("/t",'')
+    .replaceAll("/n",'')
+
+
+{
+console.log (pokemon)
+pokemonListEl.innerHTML += `
+    <article class="card ${tag}">
+        <span class="number">#${id}</span>
+        <h2>${name}</h2>
+        <img src
+    
+    
+    
+    
+    `
+}
