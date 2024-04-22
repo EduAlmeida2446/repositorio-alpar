fe6106cf4d197775e88a6f34affcb7a879eb686d
Eduardo Almeida <eduardoalmeida011@hotmail.com>
2024-04-22 13:47:00 -0300

    atvTempo

---
+++ atvdTempo/scripts/app.js
@@ -0,0 +1,9 @@
+const apiKey = '307a495d19863242c0a82bdf0150370f';
+const weatherService = new WeatherService(apiKey);
+const weatherController = new WeatherController(weatherService);
+weatherController.getWeatherByCity("São Paulo");
+
+function getWeather() {
+    const cityInput = document.getElementById('cityInput').value;
+    weatherController.getWeatherByCity(cityInput);
+}
